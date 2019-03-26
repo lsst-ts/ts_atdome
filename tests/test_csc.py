74c4fd63bfa5905d4845a0f4b8dad15140540c9f
Russell Owen <rowen@uw.edu>
2019-03-26 10:47:24 -0700

    Lengthen timeout to wait for process to end

--- tests/test_csc.py
+++ tests/test_csc.py
@@ -494,7 +494,7 @@ class CscTestCase(unittest.TestCase):
                 summaryState_data = await remote.evt_summaryState.next(flush=False, timeout=10)
                 self.assertEqual(summaryState_data.summaryState, salobj.State.OFFLINE)
 
-                await asyncio.wait_for(process.wait(), 2)
+                await asyncio.wait_for(process.wait(), 5)
             except Exception:
                 if process.returncode is None:
                     process.terminate()
